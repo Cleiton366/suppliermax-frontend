27f05446ca188ad2c7d56c16cc50dfa15a5d999d
Cleiton <cleiton.queiroz@proton.me>
2024-06-29 23:06:24 -0300

    chore: removed console.log

--- src/components/table-view.tsx
+++ src/components/table-view.tsx
@@ -194,7 +194,6 @@ export function TableView() {
   });
 
   useEffect(() => {
-    console.log("screenWidth", screenWidth);
     function setSize() {
       setScreenWidth(window.innerWidth);
     }
